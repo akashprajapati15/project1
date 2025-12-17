e69922bf39bbb5bcaded32151199dfc0861622b6
akashprajapati15 <akashprajapati96859@gmail.com>
2025-12-17 22:08:45 +0530

    creates routers product owner user

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
-
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
+
+
+const db = require("./config/mongoose-connection");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -10,8 +15,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
 
-app.get("/",(req,res)=>{
-    res.send("hey");
-});
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
+// app.get("/",(req,res)=>{
+//     res.send("hey");
+// });
 
 app.listen(3000);
--- models/owners-model.js
+++ models/owners-model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // mongoose.connect("mongodb://127.0.0.1:27017/scatch");
 
-const userSchema = mongoose.Schema({
+const ownerSchema = mongoose.Schema({
     fullname: {
         type:String,
         minlenght:3,
@@ -10,19 +10,15 @@ const userSchema = mongoose.Schema({
     },
     email: String,
     password: String,
-    cart: {
-        type: Array,
-        default: []
-    },
-    isadmin: Boolean,
-    orders: {
+    
+    products: {
         type: Array,
         default: []
     },
     contact: Number,
-    picture: String,
+    gstin: String,
 
 
 });
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("owner", ownerSchema);
