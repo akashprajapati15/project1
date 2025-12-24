85535117bd27b3f019ddf9445bb95a71b72de554
akashprajapati15 <akashprajapati96859@gmail.com>
2025-12-24 14:34:13 +0530

    creates routers product owner user

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req , res ){
-    res.send("helooooo");
+    res.send("helooooo product");
 });
 
 module.exports = router;
